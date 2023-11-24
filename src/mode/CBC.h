33c597b1a5fcb089b17d9d88a77e2ef6053e57e6
cryptogus <wnelem1234@naver.com>
2023-11-25 02:36:27 +0900

    Modify size_t to int about blocksize

--- src/mode/CBC.h
+++ src/mode/CBC.h
@@ -3,5 +3,5 @@
 #include <stdint.h>
 #include <string.h>
 
-void CBC_enc(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint8_t *key, size_t block_size, size_t len, unsigned char *src, unsigned char *des) {
-void CBC_dec(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint8_t *key, size_t block_size, size_t len, unsigned char *src, unsigned char *des) {
+void CBC_enc(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint8_t *key, int block_size, size_t len, unsigned char *src, unsigned char *des);
+void CBC_dec(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint8_t *key, int block_size, size_t len, unsigned char *src, unsigned char *des);
--- src/mode/ECB.h
+++ src/mode/ECB.h
@@ -3,4 +3,4 @@
 #include <stdint.h>
 #include <string.h>
 
-void ECB(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *key, size_t block_size, size_t len, unsigned char *src, unsigned char *des);
+void ECB(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *key, int block_size, size_t len, unsigned char *src, unsigned char *des);
