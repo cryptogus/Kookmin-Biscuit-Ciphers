f6122bf485ae4af4f4139bc44c09bf74312396ef
cryptogus <wnelem1234@naver.com>
2023-11-25 16:04:12 +0900

    Add api.h

--- apps/api.h
+++ apps/api.h
@@ -1,6 +1,14 @@
 #pragma once
 
-#include "pkcs7.h"
-#include "aes.h"
-#include "CBC.h"
-#include "ECB.h"
+#include <stdint.h>
+
+void AES128_Encrypt(uint8_t cipherText[16] ,uint8_t plainText[16], uint8_t key[16]);
+void AES128_Decrypt(uint8_t cipherText[16] ,uint8_t plainText[16], uint8_t key[16]);
+
+unsigned char *pkcs7_padding(unsigned char *input, size_t block_size, size_t *len);
+unsigned char *pkcs7_depadding(unsigned char *input, size_t *len);
+
+void ECB(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *key, size_t block_size, size_t len, unsigned char *src, unsigned char *des);
+
+void CBC_enc(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint8_t *key, size_t block_size, size_t len, unsigned char *src, unsigned char *des);
+void CBC_dec(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint8_t *key, size_t block_size, size_t len, unsigned char *src, unsigned char *des);
