7d1f862e78b5ae9093c7cd330905764c9a1c637a
cryptogus <wnelem1234@naver.com>
2023-11-26 22:47:33 +0900

    Change 16 to strlen(key)

--- src/mode/CBC.c
+++ src/mode/CBC.c
@@ -2,13 +2,13 @@
 
 void CBC_enc(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint8_t *key, size_t block_size, size_t len, unsigned char *src, unsigned char *des) {
 
-    uint8_t key2[16];
+    uint8_t key2[32];
 	
 	
     for (size_t i = 0; i < len/block_size; i++)
     {
-        //AES 키 바이트 16 -> 만약 AES key 크기가 달라지면 16을 바꿔야함
-        memcpy(key2, key, 16); // key 변수만 사용하면 on-the-fly로 encryption 코드를 짜서 key값이 계속 갱신되어 마스터키 값이 달라짐
+        //AES-128 키 바이트 16 -> 만약 AES key 크기가 달라지면 16을 바꿔야함, strlen(key)
+        memcpy(key2, key, strlen(key)); // key 변수만 사용하면 on-the-fly로 encryption 코드를 짜서 key값이 계속 갱신되어 마스터키 값이 달라짐
         int tmp = (i << 4);
         for (int j = 0; j < 16; j++)
 	    {
@@ -26,13 +26,13 @@ void CBC_enc(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint
 
 void CBC_dec(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint8_t *key, size_t block_size, size_t len, unsigned char *src, unsigned char *des) {
 
-    uint8_t key2[16];
+    uint8_t key2[32];
 	
 	
     for (size_t i = 0; i < len/block_size; i++)
     {
-        //AES 키 바이트 16 -> 만약 AES key 크기가 달라지면 16을 바꿔야함
-        memcpy(key2, key, 16); // key 변수만 사용하면 on-the-fly로 encryption 코드를 짜서 key값이 계속 갱신되어 마스터키 값이 달라짐
+        
+        memcpy(key2, key, strlen(key));
         
         int tmp = (i << 4);
         
--- src/mode/ECB.c
+++ src/mode/ECB.c
@@ -3,12 +3,13 @@
 //ECB운용모드
 void ECB(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *key, size_t block_size, size_t len, unsigned char *src, unsigned char *des) {
 
-    uint8_t key2[16];
+    // 256-bit 까지 사용가능하도록 넉넉하게 잡음
+    uint8_t key2[32];
 
     for (size_t i = 0; i < len/block_size; i++)
     {
-        //AES 키 바이트 16 -> 만약 AES key 크기가 달라지면 16을 바꿔야함
-        memcpy(key2, key, 16); // key 변수만 사용하면 on-the-fly로 encryption 코드를 짜서 key값이 계속 갱신되어 마스터키 값이 달라짐
+        //AES 키 바이트 16 -> strlen(key)
+        memcpy(key2, key, strlen(key)); // key 변수만 사용하면 on-the-fly로 encryption 코드를 짜서 key값이 계속 갱신되어 마스터키 값이 달라짐
         cipher(des + (i << 4) ,src + (i << 4), key2);
     }
 }
