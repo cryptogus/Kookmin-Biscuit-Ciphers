da3a38903e21fae0079b403386ebe22f5302e86a
cryptogus <wnelem1234@naver.com>
2023-11-26 23:30:33 +0900

    Add operation of decryption

--- apps/openKBC.c
+++ apps/openKBC.c
@@ -7,7 +7,8 @@
 void usage()
 {
     printf("Usage: openKBC [options] <cipher> <mode> <plaintext or ciphertext> <key> <iv>...\n");
-    printf("Sample: openKBC -e aes128 ecb \"With great power comes great responsibility\" spiderman0000000 0\n\n");
+    printf("Sample1: openKBC -e aes128 ecb \"With great power comes great responsibility\" spiderman0000000 0\n");
+    printf("Sample2: openKBC -e aes128 cbc \"With great power comes great responsibility\" spiderman0000000 01234567890123456\n\n");
     printf("-h is help option\n");
 }
 
@@ -28,6 +29,14 @@ void help()
 
 }
 
+// 16진수 문자열을 2바이트씩 분할하고 1바이트로 변환하는 함수
+// 문자열일 때: 8d50edd3acb65e62af313df0986ac3a105cc1d264d2f51d3e2b39f6275af871985c22ac7ffb0962322c101923d4cb675
+// 배열에서 1바이트로 사용할 때: 8d 50 ed d3 ac b6 5e 62 af 31 3d f0 98 6a c3 a1 05 cc 1d 26 4d 2f 51 d3 e2 b3 9f 62 75 af 87 19 85 c2 2a c7 ff b0 96 23 22 c1 01 92 3d 4c b6 75
+void hexStringToBytes(const char* hexString, unsigned char* bytes, size_t length) {
+    for (size_t i = 0; i < length; i++) {
+        sscanf(hexString + 2 * i, "%2hhx", &bytes[i]);
+    }
+}
 
 int main(int argc, char *argv[]) {
     
@@ -41,14 +50,21 @@ int main(int argc, char *argv[]) {
         perror("Error opening files");
         exit(EXIT_FAILURE);
     }*/
-    if (strcmp(argv[1], "-h") == 0 && argc == 2) {
+    if (argc == 1) {
+        usage();
+        return 1;
+    }
+
+    if (strcmp(argv[1], "-h") == 0) {
         help();
         return -1;
     }
+
     if (argc < 6) {
         usage();
         return 1;
     }
+
     if (strcmp(argv[1], "-e") == 0) {
         goto enc;
 
@@ -59,14 +75,13 @@ int main(int argc, char *argv[]) {
         usage();
         return 2;
     }
-
+    // 함수 포인터로 필요한 block cipher 함수 받아옴
+    void (*cipher)(uint8_t *, uint8_t *, uint8_t *);
+    
     enc:
-        // 함수 포인터로 필요한 block cipher 함수 받아옴
-        void (*cipher)(uint8_t *, uint8_t *, uint8_t *);
-        
         // pkcs7 padding
         size_t len = 0;
-        unsigned char *pad_plainText = pkcs7_padding(argv[4], BLOCK_SIZE, &len);
+        unsigned char *pad_plainText = pkcs7_padding((unsigned char *)argv[4], BLOCK_SIZE, &len);
         unsigned char *cipherText = calloc(sizeof(unsigned char), len);
 
         if (strcmp(argv[2], "aes128") == 0){
@@ -78,10 +93,10 @@ int main(int argc, char *argv[]) {
 
         if (strcmp(argv[3], "ecb") == 0){
             // argv[5]에 16자리 문자열을 입력하면 실제 메모리에는 '\0' 포함 17-byte를 사용하지만 ECB 함수에서는 새로운 변수에 키 길이만큼(strlen 사용)만 사용하기에 그대로 인자로 넘겨주기에 문제 없음
-            ECB(cipher, argv[5],BLOCK_SIZE, len, pad_plainText, cipherText);
+            ECB(cipher, (unsigned char *)argv[5],BLOCK_SIZE, len, pad_plainText, cipherText);
 
         }else if (strcmp(argv[3], "cbc") == 0){
-            CBC_enc(cipher, argv[6], argv[5],BLOCK_SIZE, len, pad_plainText, cipherText);
+            CBC_enc(cipher, (unsigned char *)argv[6], (unsigned char *)argv[5],BLOCK_SIZE, len, pad_plainText, cipherText);
         }else {
             usage();
             return 4;
@@ -92,10 +107,49 @@ int main(int argc, char *argv[]) {
             printf("%02x", cipherText[i]);
         }
         printf("\n");
+
         free(pad_plainText);
         free(cipherText);
+        
+        return 0;
 
     dec:
+        // 2바이트씩 분할한 결과를 저장할 배열
+        size_t hexStringLength = strlen(argv[4]);
+        size_t byteLength = hexStringLength / 2;
+        unsigned char *cipherText2 = (unsigned char*)malloc(byteLength);
+        unsigned char *decPlainText = calloc(sizeof(unsigned char), byteLength);
 
-    return 0;
+        // 16진수 문자열을 2바이트씩 분할하고 1바이트로 변환
+        hexStringToBytes(argv[4], cipherText2, byteLength);
+        
+        if (strcmp(argv[2], "aes128") == 0){
+            cipher = AES128_Decrypt;
+        }else{
+            usage();
+            return 3;
+        }
+
+        if (strcmp(argv[3], "ecb") == 0){
+            ECB(cipher, (unsigned char *)argv[5],BLOCK_SIZE, byteLength, cipherText2, decPlainText);
+        }else if (strcmp(argv[3], "cbc") == 0){
+            CBC_dec(cipher, (unsigned char *)argv[6], (unsigned char *)argv[5],BLOCK_SIZE, byteLength, cipherText2, decPlainText);
+        }else {
+            usage();
+            return 4;
+        }
+        /*for (size_t i = 0; i < byteLength; i++){
+            printf("%02x ", decPlainText[i]);
+        }*/
+        decPlainText = pkcs7_depadding(decPlainText, &byteLength);
+        /*for (size_t i = 0; i < byteLength; i++){
+            printf("%02x ", decPlainText[i]);
+        }*/
+        printf("plaintext: ");
+        printf("%s\n", decPlainText);
+
+        free(cipherText2);
+        free(decPlainText);
+
+        return 0;
 }
