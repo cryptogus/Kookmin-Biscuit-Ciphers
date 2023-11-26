4d886572789c886d0fc0022005bbbc520b7d3369
cryptogus <wnelem1234@naver.com>
2023-11-26 23:45:48 +0900

    Modify 16 to block_size

--- src/mode/CBC.c
+++ src/mode/CBC.c
@@ -10,14 +10,14 @@ void CBC_enc(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint
         //AES-128 키 바이트 16 -> 만약 AES key 크기가 달라지면 16을 바꿔야함, strlen(key)
         memcpy(key2, key, strlen(key)); // key 변수만 사용하면 on-the-fly로 encryption 코드를 짜서 key값이 계속 갱신되어 마스터키 값이 달라짐
         int tmp = (i << 4);
-        for (int j = 0; j < 16; j++)
+        for (int j = 0; j < block_size; j++)
 	    {
 		    src[tmp + j] ^= IV[j];
 	    }
         
         cipher(des + tmp ,src + tmp, key2);
         
-        for (int j = 0; j < 16; j++)
+        for (int j = 0; j < block_size; j++)
 	    {
 		    IV[j] = des[j + tmp];
 	    }
@@ -38,7 +38,7 @@ void CBC_dec(void (*cipher)(uint8_t *, uint8_t *, uint8_t *) , uint8_t *IV, uint
         
         cipher(des + tmp , src + tmp, key2);
         
-        for (int j = 0; j < 16; j++)
+        for (int j = 0; j < block_size; j++)
 	    {
 		    des[j + tmp] ^= IV[j];
             IV[j] = src[j + tmp];
