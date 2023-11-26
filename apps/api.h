5498bf708d97945b2f6e43eb81d02a623e330cff
cryptogus <wnelem1234@naver.com>
2023-11-26 22:26:12 +0900

    Add const blcok size

--- apps/api.h
+++ apps/api.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#define BLOCK_SIZE 16
 
 void AES128_Encrypt(uint8_t cipherText[16] ,uint8_t plainText[16], uint8_t key[16]);
 void AES128_Decrypt(uint8_t cipherText[16] ,uint8_t plainText[16], uint8_t key[16]);
