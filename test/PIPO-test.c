671fea728314f572edf822136c8a60942e79ebd1
cryptogus <wnelem1234@naver.com>
2023-11-27 13:49:00 +0900

    Add pipo test

---
+++ test/PIPO-test.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "pipo.h"
+
+u32 MASTER_KEY[2 * INT_NUM] = { 0, };
+u32 ROUND_KEY[(13 + 1) * INT_NUM] = { 0, };
+u32 PLAIN_TEXT[SIZE] = { 0, };
+u32 CIPHER_TEXT[SIZE] = { 0, };
+
+int main(void)
+{
+    unsigned char *plainText;
+	unsigned char *enc_key;
+    unsigned char *dec_key;
+    unsigned char *cipherText;
+	
+    // Test Vector
+    PLAIN_TEXT[0] = 0x1E270026;
+	PLAIN_TEXT[1] = 0x098552F6;
+    
+    MASTER_KEY[0] = 0x2E152297;
+	MASTER_KEY[1] = 0x7E1D20AD;
+	MASTER_KEY[2] = 0x779428D2;
+	MASTER_KEY[3] = 0x6DC416DD;
+
+    // 구현한 PIPO의 암복호화 결과를 담을 배열
+    unsigned char usr_plainText[8] = {0,};
+    unsigned char usr_cipherText[8] = {0,};
+    plainText = (u8*)PLAIN_TEXT;
+
+    printf("[ plain text ] - ");
+	for (int i = 0; i < 8; i++) {
+		printf("%02x ", plainText[i]);
+	}
+	printf("\n\n");
+    
+    enc_key = (u8*)MASTER_KEY;
+    dec_key = (u8*)MASTER_KEY;
+	printf("[     key    ] - ");
+	for (int i = 0; i < 16; i++) {
+		printf("%02x ", enc_key[i]);
+	}
+	printf("\n\n=================================================================\n");
+
+    PIPO128_ENC((u32*)usr_cipherText, (u32*)plainText, MASTER_KEY);
+
+    printf("PIPO128 Encryption success\n\n");
+    printf("Encryption result: ");
+    for (int i = 0; i < 8; i++) {
+		printf("%02x ", usr_cipherText[i]);
+	}
+    printf("\n=================================================================\n");
+    PIPO128_DEC((u32*)usr_plainText, (u32*)usr_cipherText, MASTER_KEY);
+
+    printf("PIPO128 Decryption success\n\n");
+    printf("Decryption result: ");
+    for (int i = 0; i < 8; i++) {
+		printf("%02x ", usr_plainText[i]);
+	}
+    printf("\n=================================================================\n");
+    return 0;
+}
