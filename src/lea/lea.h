bd79d9922fdba8db75c7837cf9f190f31a47ae58
cryptogus <wnelem1234@naver.com>
2023-11-29 03:28:51 +0900

    Add lea128 enc func

--- src/lea/lea.h
+++ src/lea/lea.h
@@ -8,3 +8,5 @@ typedef uint32_t word;
 //Little Endian
 #define GETU32(pt) (((word)(pt)[3] << 24) ^ ((word)(pt)[2] << 16) ^ ((word)(pt)[1] <<  8) ^ ((word)(pt)[0]))
 #define PUTU32(ct, st) { (ct)[3] = (byte)((st) >> 24); (ct)[2] = (byte)((st) >> 16); (ct)[1] = (byte)((st) >>  8); (ct)[0] = (byte)(st); }
+
+void LEA128_ENC(word ciphertext[], word plaintext[], word Key[]);
