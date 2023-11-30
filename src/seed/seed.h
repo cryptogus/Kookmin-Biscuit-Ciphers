d88d861278023c5834fce06790fd05eb88f46f1b
cryptogus <wnelem1234@naver.com>
2023-11-30 20:49:31 +0900

    Add block cipher seed

---
+++ src/seed/seed.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef unsigned char byte;
+typedef uint32_t word;
+
+void SEED_Enc(uint32_t* Ciphertext, uint32_t* Plaintext, uint32_t* Key);
+void SEED_Enc(uint32_t* Ciphertext, uint32_t* Plaintext, uint32_t* Key);
