4cc3b89413ec771d060a5019d180daf65aacfb69
cryptogus <wnelem1234@naver.com>
2023-11-30 20:53:58 +0900

    Fix seed build error

--- src/seed/seed.h
+++ src/seed/seed.h
@@ -6,4 +6,4 @@ typedef unsigned char byte;
 typedef uint32_t word;
 
 void SEED_Enc(uint32_t* Ciphertext, uint32_t* Plaintext, uint32_t* Key);
-void SEED_Enc(uint32_t* Ciphertext, uint32_t* Plaintext, uint32_t* Key);
+void SEED_Dec(uint32_t* Ciphertext, uint32_t* Plaintext, uint32_t* Key);
