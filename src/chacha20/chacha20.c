e84aaadd7a0c66b414374286fe649a2eafdca85d
cryptogus <wnelem1234@naver.com>
2024-02-09 10:21:05 +0900

    Complete chacha20 encryption test

--- src/chacha20/chacha20.c
+++ src/chacha20/chacha20.c
@@ -39,7 +39,7 @@ static inline void chacha20_inner_block(uint32_t *state) {
     }
 }
 
-void chacha20_block(uint32_t *block) {
+void chacha20_block(uint32_t *block, uint32_t *output) {
     uint32_t tmp[16];
     for (int i = 0; i < 16; i++) {
         tmp[i] = block[i];
@@ -50,9 +50,40 @@ void chacha20_block(uint32_t *block) {
 
     // Add the unscrambled block to the scrambled block
     for (int i = 0; i < 16; i++) {
-        block[i] += tmp[i];
+        output[i] = block[i] + tmp[i];
     }
+}
 
-    // Increasing block counter
-    //block[12]++;
-}   
+void chacha20_encrypt(uint32_t *key, uint32_t counter, uint32_t *nonce, uint32_t *src, uint32_t *dst, uint64_t len) {
+    // constants
+    uint32_t key_stream[16] = { 0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, };
+    // key
+    key_stream[4] = key[0]; key_stream[5] = key[1]; key_stream[6] = key[2]; key_stream[7] = key[3];
+    key_stream[8] = key[4]; key_stream[9] = key[5]; key_stream[10] = key[6]; key_stream[11] = key[7];
+    // block counter
+    key_stream[12] = counter;
+    // nonce
+    key_stream[13] = nonce[0]; key_stream[14] = nonce[1]; key_stream[15] = nonce[2];
+    
+    // plaintext 를 64 바이트씩 블록단위로 XOR 연산을 이용해 암호화
+    // ciphertext를 64 바이트씩 블록단위로 XOR 연산을 이용해 복호화
+    for (uint64_t i = 0; i < len/64; i++) {
+        
+        uint32_t tmp[16] = {0,};
+        chacha20_block(key_stream, tmp);
+        for (uint64_t j = 0; j < 16; j++)
+            dst[j + i*(uint64_t)16] = src[j + i*(uint64_t)16] ^ tmp[j];
+        // Increasing block counter
+        key_stream[12]++;
+    }
+    if (len%64 != 0) {
+        uint32_t tmp[16] = {0,};
+        uint64_t i = len/64;
+        chacha20_block(key_stream, tmp);
+        for (uint64_t j = 0; j <= (len%64)/4; j++)
+            dst[j + i*(uint64_t)16] = src[j + i*(uint64_t)16] ^ tmp[j];
+    }
+    // initialization
+    // Actually, the way I implemented it, it doesn't need to be done, but depending on how you implement the function, you may need to add it.
+    key_stream[12] = counter;
+}
