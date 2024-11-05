7374d931ab2587fc25d8e95718c86d66edd00383
hhlee <hhlee@ictk.com>
2024-11-05 16:42:22 +0900

    provider:first class

--- provider/KBCprov.c
+++ provider/KBCprov.c
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0 AND MIT
 
 /*
- * OQS OpenSSL 3 provider
+ * OpenSSL 3 provider
  *
- * Code strongly inspired by OpenSSL legacy provider.
- *
+ * @ref https://docs.openssl.org/3.0/man3/OSSL_DISPATCH/
+ * @ref https://docs.openssl.org/3.0/man7/provider/
+ * @ref https://docs.openssl.org/3.0/man7/provider-cipher/
+ * @ref https://github.com/open-quantum-safe/oqs-provider/blob/main/oqsprov
  */
 
 #include <errno.h>
@@ -17,3 +19,10 @@
 #include <openssl/provider.h>
 #include <stdio.h>
 #include <string.h>
+
+// aes example
+static OSSL_FUNC_cipher_newctx_fn kbc_newctx;
+OSSL_DISPATCH openkbc[] = {
+    {OSSL_FUNC_CIPHER_NEWCTX, kbc_newctx},
+
+};
